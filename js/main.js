885a42deba4d0aea2a0071a97c05ca310f2e116a
bhojani3192 <dhrumilb24@gmail.com>
2022-06-16 22:41:50 +0530

    revio
    2 pages

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// const toolTip = document.getElementById("tooltip");
+//   const info = document.getElementById("info");
+//   info.onclick = function () {
+//     if (toolTip.style.display !== "none") 
+//     toolTip.style.display = "block";
+    
+//   };
